eb1f1ac356793c0f0498427f379ab3317ab4a3e5
renancloudwalk <renan@cloudwalk.io>
2022-04-28 19:55:26 -0300

    feat: add ClientTest library with a base test
    uses reqwest for requests and TcpListener for the server

---
+++ src/lib.rs
@@ -0,0 +1,188 @@
+/// this is a hard copy from TestClient at axum
+use axum::{body::HttpBody, BoxError};
+use bytes::Bytes;
+use http::{
+    header::{HeaderName, HeaderValue},
+    Request, StatusCode,
+};
+use hyper::{Body, Server};
+use std::convert::TryFrom;
+use std::net::{SocketAddr, TcpListener};
+use tower::make::Shared;
+use tower_service::Service;
+
+pub struct TestClient {
+    client: reqwest::Client,
+    addr: SocketAddr,
+}
+
+impl TestClient {
+    pub(crate) fn new<S, ResBody>(svc: S) -> Self
+    where
+        S: Service<Request<Body>, Response = http::Response<ResBody>>
+        + Clone
+        + Send
+        + 'static,
+        ResBody: HttpBody + Send + 'static,
+        ResBody::Data: Send,
+        ResBody::Error: Into<BoxError>,
+        S::Future: Send,
+        S::Error: Into<BoxError>,
+    {
+        let listener =
+            TcpListener::bind("127.0.0.1:0").expect("Could not bind ephemeral socket");
+        let addr = listener.local_addr().unwrap();
+        println!("Listening on {}", addr);
+
+        tokio::spawn(async move {
+            let server = Server::from_tcp(listener).unwrap().serve(Shared::new(svc));
+            server.await.expect("server error");
+        });
+
+        let client = reqwest::Client::builder()
+            .redirect(reqwest::redirect::Policy::none())
+            .build()
+            .unwrap();
+
+        TestClient { client, addr }
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn get(&self, url: &str) -> RequestBuilder {
+        RequestBuilder {
+            builder: self.client.get(format!("http://{}{}", self.addr, url)),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn head(&self, url: &str) -> RequestBuilder {
+        RequestBuilder {
+            builder: self.client.head(format!("http://{}{}", self.addr, url)),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn post(&self, url: &str) -> RequestBuilder {
+        RequestBuilder {
+            builder: self.client.post(format!("http://{}{}", self.addr, url)),
+        }
+        .header("Content-Type", "application/json")
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn put(&self, url: &str) -> RequestBuilder {
+        RequestBuilder {
+            builder: self.client.put(format!("http://{}{}", self.addr, url)),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn patch(&self, url: &str) -> RequestBuilder {
+        RequestBuilder {
+            builder: self.client.patch(format!("http://{}{}", self.addr, url)),
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub(crate) struct RequestBuilder {
+    builder: reqwest::RequestBuilder,
+}
+
+impl RequestBuilder {
+    #[allow(dead_code)]
+    pub(crate) async fn send(self) -> TestResponse {
+        TestResponse {
+            response: self.builder.send().await.unwrap(),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn body(mut self, body: impl Into<reqwest::Body>) -> Self {
+        self.builder = self.builder.body(body);
+        self
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn json<T>(mut self, json: &T) -> Self
+    where
+        T: serde::Serialize,
+    {
+        self.builder = self.builder.json(json);
+        self
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn header<K, V>(mut self, key: K, value: V) -> Self
+    where
+        HeaderName: TryFrom<K>,
+    <HeaderName as TryFrom<K>>::Error: Into<http::Error>,
+        HeaderValue: TryFrom<V>,
+    <HeaderValue as TryFrom<V>>::Error: Into<http::Error>,
+    {
+        self.builder = self.builder.header(key, value);
+        self
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn multipart(mut self, form: reqwest::multipart::Form) -> Self {
+        self.builder = self.builder.multipart(form);
+        self
+    }
+}
+
+#[allow(dead_code)]
+pub(crate) struct TestResponse {
+    response: reqwest::Response,
+}
+
+impl TestResponse {
+    #[allow(dead_code)]
+    pub(crate) async fn text(self) -> String {
+        self.response.text().await.unwrap()
+    }
+
+    #[allow(dead_code)]
+    pub(crate) async fn json<T>(self) -> T
+    where
+        T: serde::de::DeserializeOwned,
+    {
+        self.response.json().await.unwrap()
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn status(&self) -> StatusCode {
+        self.response.status()
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn headers(&self) -> &http::HeaderMap {
+        self.response.headers()
+    }
+
+    #[allow(dead_code)]
+    pub(crate) async fn chunk(&mut self) -> Option<Bytes> {
+        self.response.chunk().await.unwrap()
+    }
+
+    #[allow(dead_code)]
+    pub(crate) async fn chunk_text(&mut self) -> Option<String> {
+        let chunk = self.chunk().await?;
+        Some(String::from_utf8(chunk.to_vec()).unwrap())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use axum::Router;
+    use axum::routing::get;
+    use http::StatusCode;
+
+    #[tokio::test]
+    async fn test_get_request() {
+        let app = Router::new().route("/", get(|| async {}));
+        let client = super::TestClient::new(app);
+        let res = client.get("/").send().await;
+        assert_eq!(res.status(), StatusCode::OK);
+    }
+}
